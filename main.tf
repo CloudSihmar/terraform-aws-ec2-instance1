f9b2ab2dad6ea6b1ef1ea0600e4b9bf67b341d2f
Sandeep Kumar <sandy.2087@gmail.com>
2023-05-11 12:47:32 +0530

    main file is modified

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ec2_example" {
       sudo apt-get update -u
       sudo apt install -y nginx
       sudo systemctl start nginx
-      sudo echo "<html><body><h1>Hello this is module-1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      sudo echo "<html><body><h1>Hello this is module-2 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
       EOF
 }
 
