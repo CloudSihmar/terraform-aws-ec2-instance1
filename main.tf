55155a039ecfdb07eb52706a0861acfe3ebef42e
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-05-18 15:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "ec2_example" {
 
     ami = var.ami
     instance_type = var.instance_type
-    key_name = "terraform"
     vpc_security_group_ids = [aws_security_group.main.id]
     associate_public_ip_address = true
 
